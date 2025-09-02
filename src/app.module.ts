251b7af048afedbbd21d64b3f8c21e2c527b25a5
mbesoftware21 <mezabetancuresteban@gmail.com>
2025-09-02 00:01:45 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ClientesModule } from './modules/clientes/clientes.module';
+import { FondosModule } from './modules/fondos/fondos.module';
+import { TransaccionesModule } from './modules/transacciones/transacciones.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './infrastructure/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ClientesModule,
+    FondosModule,
+    TransaccionesModule,
+    AuthModule,
+    DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
